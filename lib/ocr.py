0af9b9cc9833fa99eec0ceae854ceb8fb7c5d428
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2018-11-05 14:28:38 -0500

    update to ocr.py, likely won't be used in favor of image searching

--- lib/ocr.py
+++ lib/ocr.py
@@ -1,4 +1,5 @@
 """
+#read image
 import pytesseract
 import requests
 from PIL import Image
@@ -15,3 +16,26 @@ def process_image(url):
 def _get_image(url):
     return Image.open(StringIO(requests.get(url).content))
 """
+
+"""
+#draw bounding boxes around text
+import cv2
+import pytesseract
+
+filename = 'image.png'
+
+# read the image and get the dimensions
+img = cv2.imread(filename)
+h, w, _ = img.shape # assumes color image
+
+# run tesseract, returning the bounding boxes
+boxes = pytesseract.image_to_boxes(img) # also include any config options you use
+
+# draw the bounding boxes on the image
+for b in boxes.splitlines():
+    b = b.split(' ')
+    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+# show annotated image and wait for keypress
+cv2.imshow(filename, img)
+cv2.waitKey(0)
