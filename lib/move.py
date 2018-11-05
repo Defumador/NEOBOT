45ca3fa3df14e2712c22673b5a09b6c8be0966de
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2018-11-05 08:23:56 -0500

    changed probabilities for mouse movement methods

--- lib/move.py
+++ lib/move.py
@@ -3,19 +3,22 @@ os.chdir('C:\\Program Files (x86)\\Python37-32\Lib\\site-packages\\') #change di
 import pyautogui #import pyautogui
 pyautogui.PAUSE = 2.5
 
-def movetime(): #randomize the amount of time mouse takes to move to a location
-    movetimevar = (random.randint(0,5000) / 1000)
-    print(movetimevar)
+def movetime(): #randomize the amount of time mouse takes to move to a new location
+    movetimevar = (random.randint(0,3000) / 1000) #take up to 3 seconds to move mouse, convert from miliseconds to seconds
     return movetimevar
 
 def mousepath(): #randomize the behavior of mouse button as it moves to a location
-    mousepathvar = (random.randint(1,5))
-    print(mousepathvar)
+    mousepathvar = (random.randint(1,6))
+    #higher chance of easeInQuad and easeOutQuad movement methods than easeInBounce or easeInElastic
     if mousepathvar == 1:
         return pyautogui.easeInQuad
     elif mousepathvar == 2:
-        return pyautogui.easeOutQuad
+        return pyautogui.easeInQuad
     elif mousepathvar == 3:
+        return pyautogui.easeOutQuad
+    elif mousepathvar == 4:
+        return pyautogui.easeOutQuad
+    elif mousepathvar == 5:
         return pyautogui.easeInBounce
     else:
         return pyautogui.easeInElastic
