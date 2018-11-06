1f8df9a9dd86ff7fa67ea6d9f3b339118cb64157
Austin <37026149+Zultair@users.noreply.github.com>
2018-11-05 20:03:56 -0500

    reorganized lib, redesigned core functions

---
+++ lib/mouse.py
@@ -0,0 +1,40 @@
+import random, os #import module to allow for Random command in ahk
+os.chdir('C:\\Program Files (x86)\\Python37-32\Lib\\site-packages\\')
+import pyautogui #import pyautogui
+os.chdir('D:\OneDrive\Documents\Scripts\Python\PY-NEOBOT-GitHub\lib')
+
+def click(): #click the mouse for a randomized period of time
+    pyautogui.PAUSE = (random.randint(0,500) / 1000) #wait up to 1 second before clicking, divide by 1000 to convert from miliseconds to seconds
+    pyautogui.mouseDown() #click mouse button down
+    pyautogui.PAUSE = (random.randint(0,100) / 1000) #hold down mouse button for up to 250ms                       
+    pyautogui.mouseUp() #release mouse button
+    pyautogui.PAUSE = (random.randint(0,500) / 1000) #wait up to 1 second after clicking
+    return
+
+def clickright(): #same thing but with right mouse button
+    pyautogui.PAUSE = (random.randint(0,1000) / 1000)
+    pyautogui.mouseDown(button='right')
+    pyautogui.PAUSE = (random.randint(0,250) / 1000)                       
+    pyautogui.mouseUp(button='right')
+    pyautogui.PAUSE = (random.randint(0,1000) / 1000)
+    return
+
+def move_time(): #randomize the amount of time mouse takes to move to a new location
+    movetimevar = (random.randint(0,3000) / 1000) #take up to 3 seconds to move mouse, convert from miliseconds to seconds
+    return movetimevar
+
+def mouse_path(): #randomize the behavior of mouse button as it moves to a location
+    mousepathvar = (random.randint(1,6))
+    #higher chance of easeInQuad and easeOutQuad movement methods than easeInBounce or easeInElastic
+    if mousepathvar == 1:
+        return pyautogui.easeInQuad
+    elif mousepathvar == 2:
+        return pyautogui.easeInQuad
+    elif mousepathvar == 3:
+        return pyautogui.easeOutQuad
+    elif mousepathvar == 4:
+        return pyautogui.easeOutQuad
+    elif mousepathvar == 5:
+        return pyautogui.easeInBounce
+    else:
+        return pyautogui.easeInElastic
---
+++ lib/while_docked.py
@@ -0,0 +1,68 @@
+import random, os, sys #import module to allow for Random command in ahk
+from mouse import *
+import pyautogui #import pyautogui
+pyautogui.PAUSE = 2.5
+os.chdir('D:\OneDrive\Documents\Scripts\Python\PY-NEOBOT-GitHub\lib')
+
+def open_station_hangar(): #click on station hangar button in inventory window while docked
+    station_hangar_icon = pyautogui.locateCenterOnScreen('inventory_station_hangar_icon.png') 
+    print(station_hangar_icon)
+    if station_hangar_icon == None:
+        print('cant find inventory_station_hangar_icon')
+        sys.exit()
+    else:
+        (station_hangar_iconx, station_hangar_icony) = station_hangar_icon
+        pyautogui.moveTo(station_hangar_iconx, station_hangar_icony, move_time(), mouse_path())  # clicks the center of where the button was found
+        click()
+        return
+
+def focus_inventory_window(): #click inside the station inventory window to focus it before items are selected
+    sorting_station_hangar_icon = pyautogui.locateCenterOnScreen('sorting_station_hangar_icon.png') #look for sorting buttons in top right corner of inventory window and offset mouse
+    print(sorting_station_hangar_icon)
+    if sorting_station_hangar_icon == None:
+        print('cant find sorting_station_hangar_icon')
+        sys.exit()
+    else:
+        (sorting_station_hangar_iconx, sorting_station_hangar_icony) = sorting_station_hangar_icon
+        pyautogui.moveTo((sorting_station_hangar_iconx - (random.randint(10, 100))), (sorting_station_hangar_icony + (random.randint(10, 100))), move_time(), mouse_path())  # clicks the center of where the button was found
+        click()
+        return
+
+def drag_items_to_cargo_bay(): #drag items from inventory into ship cargo bay
+    namefield_station_hangar_icon = pyautogui.locateCenterOnScreen('namefield_station_hangar_icon.png') #look for 'name' column header at top of inventory window and offset mouse
+    print(namefield_station_hangar_icon)
+    if namefield_station_hangar_icon == None:
+        print('cant find namefield_station_hangar_icon')
+        sys.exit()
+    else:
+        inventory_current_ship_icon = pyautogui.locateCenterOnScreen('inventory_current_ship_icon.png') #look for ship cargo bay icon in inventory sidebar
+        print(inventory_current_ship_icon)
+        if inventory_current_ship_icon == None:
+            print('cant find inventory_current_ship_icon')
+            sys.exit()
+        else:
+            (namefield_station_hangar_iconx, namefield_station_hangar_icony) = namefield_station_hangar_icon
+            (inventory_current_ship_iconx, inventory_current_ship_icony) = inventory_current_ship_icon
+            pyautogui.moveTo(namefield_station_hangar_iconx, namefield_station_hangar_icony, move_time(), mouse_path())
+            pyautogui.PAUSE = (random.randint(0,1000) / 1000) #wait up to 1 second before clicking, divide by 1000 to convert from miliseconds to seconds
+            pyautogui.mouseDown() 
+            pyautogui.PAUSE = (random.randint(0,1000) / 1000)
+            pyautogui.moveTo(inventory_current_ship_iconx, inventory_current_ship_icony, move_time(), mouse_path())
+            pyautogui.PAUSE = (random.randint(0,1000) / 1000)
+            pyautogui.mouseUp()
+            pyautogui.PAUSE = (random.randint(0,1000) / 1000)
+            return
+
+def undock():
+    undock_icon = pyautogui.locateCenterOnScreen('undock_icon.png')
+    print(undock_icon)
+    if undock_icon == None:
+        print('cant find undock_icon')
+        sys.exit()
+    else:
+        print('undocking')
+        (undock_iconx, undock_icony) = undock_icon
+        pyautogui.moveTo(undock_iconx, undock_icony, move_time(), mouse_path())  # clicks the center of where the button was found
+        click()
+        return
+    
