6c8d9060af42c2e01b0e8f079b2133438c426e18
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2018-11-07 12:05:58 -0500

    comments

--- lib/while_docked.py
+++ lib/while_docked.py
@@ -6,7 +6,6 @@ os.chdir('D:\OneDrive\Documents\Scripts\Python\PY-NEOBOT-GitHub\lib')
 
 def open_station_hangar(): #click on station hangar button in inventory window while docked
     station_hangar_icon = pyautogui.locateCenterOnScreen('inventory_station_hangar_icon.png') 
-    print(station_hangar_icon)
     if station_hangar_icon == None:
         print('cant find inventory_station_hangar_icon')
         sys.exit()
@@ -18,7 +17,6 @@ def open_station_hangar(): #click on station hangar button in inventory window w
 
 def focus_inventory_window(): #click inside the station inventory window to focus it before items are selected
     sorting_station_hangar_icon = pyautogui.locateCenterOnScreen('sorting_station_hangar_icon.png') #look for sorting buttons in top right corner of inventory window and offset mouse
-    print(sorting_station_hangar_icon)
     if sorting_station_hangar_icon == None:
         print('cant find sorting_station_hangar_icon')
         sys.exit()
@@ -30,18 +28,16 @@ def focus_inventory_window(): #click inside the station inventory window to focu
 
 def drag_items_to_cargo_bay(): #drag items from inventory into ship cargo bay
     namefield_station_hangar_icon = pyautogui.locateCenterOnScreen('namefield_station_hangar_icon.png') #look for 'name' column header at top of inventory window and offset mouse
-    print(namefield_station_hangar_icon)
     if namefield_station_hangar_icon == None:
         print('cant find namefield_station_hangar_icon')
         sys.exit()
     else:
-        inventory_current_ship_icon = pyautogui.locateCenterOnScreen('inventory_current_ship_icon.png') #look for ship cargo bay icon in inventory sidebar
-        print(inventory_current_ship_icon)
+        inventory_current_ship_icon = pyautogui.locateCenterOnScreen('inventory_current_ship_icon.png') #if icon found, look for ship cargo bay icon in inventory sidebar
         if inventory_current_ship_icon == None:
             print('cant find inventory_current_ship_icon')
             sys.exit()
-        else:
-            (namefield_station_hangar_iconx, namefield_station_hangar_icony) = namefield_station_hangar_icon
+        else: #if found icons, click on first item in station hangar and drag mouse to ship cargo bay
+            (namefield_station_hangar_iconx, namefield_station_hangar_icony) = namefield_station_hangar_icon 
             (inventory_current_ship_iconx, inventory_current_ship_icony) = inventory_current_ship_icon
             pyautogui.moveTo(namefield_station_hangar_iconx, namefield_station_hangar_icony, move_time(), mouse_path())
             pyautogui.PAUSE = (random.randint(0,1000) / 1000) #wait up to 1 second before clicking, divide by 1000 to convert from miliseconds to seconds
@@ -55,7 +51,6 @@ def drag_items_to_cargo_bay(): #drag items from inventory into ship cargo bay
 
 def undock():
     undock_icon = pyautogui.locateCenterOnScreen('undock_icon.png')
-    print(undock_icon)
     if undock_icon == None:
         print('cant find undock_icon')
         sys.exit()
