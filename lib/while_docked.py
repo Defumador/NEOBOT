da3e1d72d0a310d9fe4b9ad93833f37de204246d
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2018-11-15 15:34:18 -0500

    began making script modular

--- lib/while_docked.py
+++ lib/while_docked.py
@@ -9,28 +9,31 @@ conf = 0.95
 
 # check if ship is docked
 def docked_check():
+    print('checking if docked')
     undock_icon = pyautogui.locateCenterOnScreen('undock_icon.png', confidence=conf)
     if undock_icon is None:
         print('not docked')
-        navigation.select_waypoint()
+        docked_check = 0
+        return docked_check
     elif undock_icon is not None:
         print('docked')
-        navigation.at_home_check()
+        docked_check = 1
+        return docked_check
 
 
 #def open_ship_cargo_bay():
 
 
 def open_station_hangar():  # click on station hangar button in inventory window while docked
+    print('opening station hangar')
     os.chdir('c:/users/austin/desktop/icons')
     station_hangar_icon = pyautogui.locateCenterOnScreen('inventory_station_hangar_icon.png',
                                                          confidence=conf)
     while station_hangar_icon is None:
-        print('cant find inventory_station_hangar_icon')
+        print('cant find inventory station hangar icon')
         station_hangar_icon = pyautogui.locateCenterOnScreen('inventory_station_hangar_icon.png',
                                                              confidence=conf)
     else:
-        print('found inventory_station_hangar_icon')
         (station_hangar_iconx, station_hangar_icony) = station_hangar_icon
         # clicks the center of where the button was found
         pyautogui.moveTo((station_hangar_iconx + (random.randint(-6, 6))),
@@ -42,15 +45,15 @@ def open_station_hangar():  # click on station hangar button in inventory window
 
 def focus_inventory_window():  # click inside the station inventory window to focus it before items are selected
     # look for sorting buttons in top right corner of inventory window and offset mouse
+    print('focusing inventory window')
     os.chdir('c:/users/austin/desktop/icons')
     sorting_station_hangar_icon = pyautogui.locateCenterOnScreen('sorting_station_hangar_icon.png',
                                                                  confidence=conf)
     while sorting_station_hangar_icon is None:
-        print('cant find sorting_station_hangar_icon')
+        print('cant find sorting icon')
         sorting_station_hangar_icon = pyautogui.locateCenterOnScreen('sorting_station_hangar_icon.png',
                                                                      confidence=conf)
     else:
-        print('found sorting_station_hangar_icon')
         (sorting_station_hangar_iconx, sorting_station_hangar_icony) = sorting_station_hangar_icon
         # offset mouse from sorting button to click within inventory window to focus it
         pyautogui.moveTo((sorting_station_hangar_iconx - (random.randint(40, 300))),
@@ -61,13 +64,13 @@ def focus_inventory_window():  # click inside the station inventory window to fo
 
 
 def undock():
+    print('began undocking procedure')
     os.chdir('c:/users/austin/desktop/icons')
     undock_icon = pyautogui.locateCenterOnScreen('undock_icon.png', confidence=conf)
     while undock_icon is None:
         print('cant find undock button')
         undock_icon = pyautogui.locateCenterOnScreen('undock_icon.png', confidence=conf)
     else:
-        print('undocking')
         (undock_iconx, undock_icony) = undock_icon
         # clicks the center of where the button was found
         pyautogui.moveTo((undock_iconx + (random.randint(-25, 25))),
@@ -77,4 +80,5 @@ def undock():
         pyautogui.moveRel((-1 * (random.randint(10, 1000))), (random.randint(40, 1000)),
                           mouse.move_time(), mouse.mouse_path())  # move mouse away from button
         time.sleep(10)  # wait for undock to complete
-        navigation.select_waypoint()
+        print('finished undocking')
+        return
