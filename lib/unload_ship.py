0685e72223e3ec56faf838a99b9a82164fb6e650
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2018-11-21 11:43:36 -0500

    removed redundant function, added random sleeps

--- lib/unload_ship.py
+++ lib/unload_ship.py
@@ -2,21 +2,18 @@ import sys, pyautogui, os, time, random, ctypes
 from lib import mouse, keyboard, while_docked, load_ship
 
 pyautogui.FAILSAFE = True
-
 sys.setrecursionlimit(100000)
 conf = 0.95
 
 
-def drag_items_from_cargo_hold():
+def drag_items_from_hold():
     # dragitems to station item hangar
-    # look for 'name' column header at top of inventory window and offset mouse
     namefield_station_hangar_icon = pyautogui.locateCenterOnScreen('namefield_station_hangar.bmp',
                                                                    confidence=conf)
     (namefield_station_hangar_iconx, namefield_station_hangar_icony) = namefield_station_hangar_icon
     pyautogui.moveTo((namefield_station_hangar_iconx + (random.randint(-5, 250))),
                      (namefield_station_hangar_icony + (random.randint(10, 25))),
                      mouse.move_time(), mouse.mouse_path())
-    # wait up to 1 second before clicking, divide by 1000 to convert from miliseconds to seconds
     pyautogui.mouseDown()
     station_hangar = pyautogui.locateCenterOnScreen('station_hangar.bmp',
                                                                    confidence=conf)
@@ -25,33 +22,7 @@ def drag_items_from_cargo_hold():
                      (station_hangary + (random.randint(-10, 10))),
                      mouse.move_time(), mouse.mouse_path())
     pyautogui.mouseUp()
-    # after unloading main cargo hold, look for special cargo hold
-    print('moved all item stacks from cargo hold')
-    return
-
-
-# drag items from inventory into ship special hold hold
-def drag_items_from_special_hold():
-    # check if ship has specialized hold that needs to be unloaded
-    print('moving all item stacks from special hold')
-   # select all items in special hold
-
-    namefield_station_hangar = pyautogui.locateCenterOnScreen('namefield_station_hangar.bmp',
-                                                                   confidence=conf)
-    (namefield_station_hangarx, namefield_station_hangary) = namefield_station_hangar
-    pyautogui.moveTo((namefield_station_hangarx + (random.randint(-5, 200))),
-                     (namefield_station_hangary + (random.randint(10, 20))),
-                     mouse.move_time(), mouse.mouse_path())
-    # wait up to 1 second before clicking, divide by 1000 to convert from miliseconds to seconds
-    pyautogui.mouseDown()
-    station_hangar = pyautogui.locateCenterOnScreen('station_hangar.bmp',
-                                                                   confidence=conf)
-    (station_hangarx, station_hangary) = station_hangar
-    pyautogui.moveTo((station_hangarx + (random.randint(-15, 40))),
-                     (station_hangary + (random.randint(-10, 10))),
-                     mouse.move_time(), mouse.mouse_path())
-    pyautogui.mouseUp()
-    print('moved all item stacks from special hold')
+    print('moved all item stacks from hold')
     return
 
 
@@ -60,31 +31,41 @@ def unload_ship():
     global unload_ship_var
     while_docked.open_cargo_hold()
     while_docked.look_for_items()
-    if while_docked.look_for_items_var == 0:  # if no items, return function
+    if while_docked.look_for_items_var == 0: 
         while_docked.look_for_special_hold()
         if while_docked.look_for_special_hold_var == 1:
+            time.sleep((random.randint(0, 200)) / 100)  # wait between 0 and 2s before actions for increased randomness
             while_docked.open_special_hold()
             while_docked.look_for_items()
             while while_docked.look_for_items_var == 1:
+                time.sleep((random.randint(0, 200)) / 100)
                 while_docked.focus_inventory_window()
+                time.sleep((random.randint(0, 200)) / 100)
                 keyboard.select_all()
-                drag_items_from_special_hold()
+                time.sleep((random.randint(0, 200)) / 100)
+                drag_items_from_hold()
                 time.sleep(2)
                 while_docked.look_for_items()
                 print('finished unloading procedure')
+                unload_ship_var = 1
                 return
             if while_docked.look_for_items_var == 0:
                 print('finished unloading procedure')
+                unload_ship_var = 1
                 return
         elif while_docked.look_for_special_hold_var == 0:
             print('error, nothing to unload')
+            unload_ship_var = 0
+            traceback.print_exc()
+            traceback.print_stack()
             sys.exit()
             return
     while while_docked.look_for_items_var == 1:
-        print('unloading items from cargo hold')
         while_docked.focus_inventory_window()
+        time.sleep((random.randint(0, 200)) / 100)
         keyboard.select_all()
-        drag_items_from_cargo_hold()
+        time.sleep((random.randint(0, 200)) / 100)
+        drag_items_from_hold()
         time.sleep(2)
         while_docked.look_for_special_hold()
         while_docked.look_for_items()
@@ -93,12 +74,16 @@ def unload_ship():
         while_docked.look_for_items()
         while while_docked.look_for_items_var == 1:
             while_docked.focus_inventory_window()
+            time.sleep((random.randint(0, 200)) / 100)
             keyboard.select_all()
-            drag_items_from_special_hold()
+            time.sleep((random.randint(0, 200)) / 100)
+            drag_items_from_hold()
             time.sleep(2)
             while_docked.look_for_items()
             print('finished unloading procedure')
+            unload_ship_var = 1
             return
     elif while_docked.look_for_special_hold_var == 0:
         print('finished unloading procedure')
+        unload_ship_var = 1
         return
