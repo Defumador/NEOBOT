597549e782155ab87003e4dcb50f207c0c13fcbd
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2019-03-19 09:50:27 -0400

    updated with intended functionality with comments

--- lib/mining.py
+++ lib/mining.py
@@ -1,11 +1,12 @@
-def check_for_hostiles()
-
-def check_for_players()
+def check_for_hostiles()  # check entire screen for red ship icons, indicating hostile vessels
 
 
-def target_ore()
+def check_for_players()  # check entire screen for other ship icons
 
-def check_hold()
 
-def activate_mining_laser()
+def target_ore()  # target closest ore in overview, if no ore is found, warp to next bookmark
+
+def check_hold()  # check cargohold to see if at capacity
+
+def activate_mining_laser()  # turn on mining lasers to mine ore
 
