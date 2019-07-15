e13e7ba4d9960239938219f858a62b89b2514952
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2019-07-15 15:05:43 -0400

    adjusted randomness parameters

--- lib/keyboard.py
+++ lib/keyboard.py
@@ -3,26 +3,17 @@ import pyautogui
 
 
 def keypress(key):
-    # Hold down the specified key for a random period of time rather than just
-    # pressing it momentarily.
-    time.sleep(float(random.randint(0, 500)) / 1000)
+    """Hold down the specified key for a random period of time rather than just
+    pressing it momentarily."""
+    time.sleep(float(random.randint(5, 500)) / 1000)
     pyautogui.keyDown(key)
-    time.sleep(float(random.randint(5, 200)) / 1000)
+    time.sleep(float(random.randint(5, 190)) / 1000)
     pyautogui.keyUp(key)
-    time.sleep(float(random.randint(0, 500)) / 1000)
-    return
-
-
-def enter():
-    print('enter -- called')
-    pyautogui.keyDown('enter')
-    time.sleep(float(random.randint(0, 500)) / 1000)
-    pyautogui.keyUp('enter')
+    time.sleep(float(random.randint(5, 500)) / 1000)
     return
 
 
 def select_all():
-    print('select_all -- called')
     pyautogui.keyDown('ctrl')
     time.sleep(float(random.randint(0, 800)) / 1000)
 
