a84f6f8d7237382d99efed0f125b0a98506e43cb
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2019-07-17 14:19:38 -0400

    Update guitest.py

--- lib/guitest.py
+++ lib/guitest.py
@@ -1,79 +1,126 @@
-'''
-from tkinter import *
-import logging
 import time
-from threading import Thread
+import threading
+import logging
+try:
+    import tkinter as tk # Python 3.x
+    import tkinter.scrolledtext as ScrolledText
+except ImportError:
+    import Tkinter as tk # Python 2.x
+    import ScrolledText
+
+class TextHandler(logging.Handler):
+    # This class allows you to log to a Tkinter Text or ScrolledText widget
+    # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
+
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        # This is necessary because we can't modify the Text from other threads
+        self.text.after(0, append)
 
 
-def test():
-    logger = logging.getLogger(
-        'print_stuff')  # will inherit "root" logger settings
-    logger.info('This will now show')
-    print('test132')
+class Frame1(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, bg="red")
+        self.parent = parent
+        self.widgets()
+        self.mainWidgets()
 
+    def widgets(self):
+        self.text = tk.Text(self)
+        self.text.insert(tk.INSERT, "Hello World\t")
+        self.text.insert(tk.END, "This is the first frame")
+        self.text.grid(row=0, column=0, padx=20, pady=20) # margins
 
-class IODirector(object):
-    def __init__(self, text_area):
-        self.text_area = text_area
+    def mainWidgets(self):
 
+        self.label1 = tk.Label(self, text="Main window label", bg="green")
+        self.label1.grid(row=3, column=0)
 
-class StdoutDirector(IODirector):
-    def write(self, msg):
-        self.text_area.insert(END, msg)
+        self.label2 = tk.Label(self, text="Main window label", bg="yellow")
+        self.label2.grid(row=4, column=0)
 
-    def flush(self):
-        pass
+        self.window = tk.Frame1(self)
+        self.window.grid(row=5, column=10, rowspan=2)
 
+class myGUI(tk.Frame):
 
-class App(Frame):
-    def __init__(self, master):
-        self.master = master
-        Frame.__init__(self, master, relief=SUNKEN, bd=2)
-        self.start()
+    # This class defines the graphical user interface 
 
-    def start(self):
-        self.master.title("Test")
-        self.submit = Button(self.master, text='Run', command=self.do_run,
-                             fg="red")
-        self.submit.grid(row=1, column=2)
-        self.text_area = Text(self.master, height=2.5, width=30,
-                              bg='light cyan')
-        self.text_area.grid(row=1, column=1)
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.build_gui()
+        self.mainWidgets()
+    
+    def mainWidgets(self):
 
-    def do_run(self):
-        t = Thread(target=print_stuff)
-        sys.stdout = StdoutDirector(self.text_area)
-        # configure the nameless "root" logger to also write           # added
-        # to the redirected sys.stdout                                 # added
-        logger = logging.getLogger()  # added
-        console = logging.StreamHandler(stream=sys.stdout)  # added
-        logger.addHandler(console)  # added
-        t.start()
+        self.label1 = tk.Label(self, text="Main window label", bg="green")
+        self.label1.grid(row=3, column=0)
 
+        self.label2 = tk.Label(self, text="Main window label", bg="yellow")
+        self.label2.grid(row=4, column=0)
 
-def print_stuff():
-    logger = logging.getLogger(
-        'print_stuff')  # will inherit "root" logger settings
-    logger.info('This will now show')  # changed
-    print('This will show')
-    time.sleep(3)
-    test()
+        #self.window = mainWidgets(self)
+        #self.window.grid(row=5, column=10, rowspan=2)
 
+    def build_gui(self):                    
+        # Build GUI
+        self.root.title('TEST')
+        self.root.option_add('*tearOff', 'FALSE')
+        self.grid(column=0, row=1, sticky='s', padx=10, pady=30)
+        self.grid_columnconfigure(0, weight=1, uniform='a')
+        self.grid_columnconfigure(1, weight=1, uniform='a')
+        self.grid_columnconfigure(2, weight=1, uniform='a')
+        self.grid_columnconfigure(3, weight=1, uniform='a')
 
-def print_some_other_stuff():
-    logger = logging.getLogger(
-        'print_some_other_stuff')  # will inherit "root" logger settings
-    logger.info('This will also now show')  # changed
-    print('This will also show')
+        # Add text widget to display logging info
+        st = ScrolledText.ScrolledText(self, state='disabled')
+        st.configure(font='TkFixedFont')
+        st.grid(column=0, row=2, sticky='s', columnspan=4)
 
+        # Create textLogger
+        text_handler = TextHandler(st)
+
+        # Logging configuration
+        logging.basicConfig(filename='test.log',
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s')        
+
+        # Add the handler to logger
+        logger = logging.getLogger()        
+        logger.addHandler(text_handler)
+
+def worker():
+    # Skeleton worker function, runs in separate thread (see below)   
+    while True:
+        # Report time / date at 2-second intervals
+        time.sleep(2)
+        timeStr = time.asctime()
+        msg = 'Current time: ' + timeStr
+        logging.info(msg) 
 
 def main():
-    logging.basicConfig(level=logging.INFO)  # enable logging           # added
-    root = Tk()
-    app = App(root)
+
+    root = tk.Tk()
+    myGUI(root)
+    #Frame1(root)
+
+    t1 = threading.Thread(target=worker, args=[])
+    t1.start()
+
     root.mainloop()
+    t1.join()
 
-
-if __name__ == '__main__':
-    main()
-'''
+main()
