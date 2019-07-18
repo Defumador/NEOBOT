c3ebbcdd0042e8958843073c1d5a73a65f017ac0
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2019-07-18 14:28:39 -0400

    created modular hotkey function

--- lib/keyboard.py
+++ lib/keyboard.py
@@ -4,24 +4,23 @@ import pyautogui
 
 
 def keypress(key):
-    """Hold down the specified key for a random period of time rather than just
-    pressing it momentarily."""
-    time.sleep(float(random.randint(5, 500)) / 1000)
+    time.sleep(float(random.randint(500, 1000)) / 1000)
     pyautogui.keyDown(key)
     time.sleep(float(random.randint(5, 190)) / 1000)
     pyautogui.keyUp(key)
-    time.sleep(float(random.randint(5, 500)) / 1000)
+    time.sleep(float(random.randint(500, 1000)) / 1000)
     return
 
 
-def select_all():
-    pyautogui.keyDown('ctrl')
-    time.sleep(float(random.randint(0, 800)) / 1000)
-
-    pyautogui.keyDown('a')
-    time.sleep(float(random.randint(0, 800)) / 1000)
-    pyautogui.keyUp('a')
-
-    time.sleep(float(random.randint(0, 800)) / 1000)
-    pyautogui.keyUp('ctrl')
+def hotkey(key1, key2):
+    """Perform a two-key hotkey shortcut, such as Ctrl-c for copying."""
+    time.sleep(float(random.randint(500, 1000)) / 1000)
+    pyautogui.keyDown(key1)
+    time.sleep(float(random.randint(10, 500)) / 1000)
+    pyautogui.keyDown(key2)
+    time.sleep(float(random.randint(10, 500)) / 1000)
+    pyautogui.keyUp(key1)
+    time.sleep(float(random.randint(10, 500)) / 1000)
+    pyautogui.keyUp(key2)
+    time.sleep(float(random.randint(500, 1000)) / 1000)
     return
