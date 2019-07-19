0396393d23c282f606ef4585da39ca77c1aaf4b3
takelley1 <37026149+Zultair@users.noreply.github.com>
2019-07-19 19:19:52 -0400

    Update docstrings
    Signed-off-by: takelley1 <37026149+Zultair@users.noreply.github.com>

--- src/mining.py
+++ src/mining.py
@@ -10,14 +10,12 @@ from src.vars import originx, originy, windowx, windowy
 
 sys.setrecursionlimit(9999999)
 
-# The number of mining modules the ship has.
-# mining_lasers = 2
-
 logging.basicConfig(format='(%(levelno)s) %(asctime)s - %(funcName)s -- %('
                            'message)s', level=logging.DEBUG)
 
 
 def activate_miner(module_num):
+    """Activates mining modules based on the number passed to this function."""
     for n in range(1, (module_num + 1)):
         keyboard.keypress('f' + (str(n)))
         logging.debug('activating miner ' + (str(n)))
@@ -29,8 +27,8 @@ def activate_miner(module_num):
 
 
 def asteroid_depleted_popup():
-    # Check for popup indicating the asteroid currently being mined has been
-    # depleted.
+    """Checks for popup indicating the asteroid currently being mined has been
+    depleted."""
     logging.debug('asteroid still available')
     return 0
 
@@ -49,8 +47,8 @@ elif asteroid_depleted_popup_var is not None:
 
 
 def ship_full_popup():
-    # Check for momentary popup indicating cargo/ore hold is full.
-    # This popup lasts about 5 seconds.
+    """Checks for momentary popup indicating that the ship's inventory is full.
+    This popup lasts about 5 seconds."""
     inv_full_popup_var = pag.locateCenterOnScreen(
         './img/popups/ship_inv_full.bmp',
         confidence=0.9,
@@ -65,9 +63,9 @@ def ship_full_popup():
 
 
 def miner_out_of_range_popup():
-    # Check if the ship's mining laser is out of range. If it is,
-    # orbit the asteroid at a specified distance and try activating the
-    # mining laser again in a few seconds.
+    """Checks if the ship's mining laser is out of range. If it is,
+    orbit the asteroid at a specified distance and try activating the
+    mining laser again in a few seconds."""
     miner_out_of_range = pag.locateCenterOnScreen(
         './img/popups/miner_out_of_range.bmp',
         confidence=0.90,
@@ -81,8 +79,9 @@ def miner_out_of_range_popup():
 
 
 def time_at_site(timer_var):
-    # Timeout timer for mining. If, for some reason, miner gets stuck in
-    # belt, restart script after a certain period of time.
+    """Timeout timer for mining. If, for some reason, ship gets stuck in
+    belt, the timer can be used to restart the script after a certain period of
+    time."""
     logging.debug('time spent at site is ' + (str(timer_var)) + 's')
     if timer_var >= 800:
         logging.warning('timed out!')
