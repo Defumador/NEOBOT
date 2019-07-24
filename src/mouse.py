54b8b18a7515c5a751fc0aefc0eb15c87ac2ab85
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2019-07-24 15:49:55 -0400

    Add bezier curve mouse movement function

--- src/mouse.py
+++ src/mouse.py
@@ -3,6 +3,46 @@ import pyautogui as pag
 from src import mouse
 from src.vars import windowx, windowy
 
+#####################################################################
+# Bezier curve movement testing
+# from https://stackoverflow.com/questions/44467329/pyautogui-mouse-movement-with-bezier-curve
+
+import scipy
+from scipy import interpolate
+
+def bezmove():
+    cp = random.randint(3, 5)  # Number of control points. Must be at least 2.
+    x1, y1 = pag.position()  # Starting position
+    x2, y2 = 444, 631  # Destination
+
+    # Distribute control points between start and destination evenly.
+    x = scipy.linspace(x1, x2, num=cp, dtype='int')
+    y = scipy.linspace(y1, y2, num=cp, dtype='int')
+
+    # Randomise inner points a bit (+-RND at most).
+    RND = 10
+    xr = scipy.random.randint(-RND, RND, size=cp)
+    yr = scipy.random.randint(-RND, RND, size=cp)
+    xr[0] = yr[0] = xr[-1] = yr[-1] = 0
+    x += xr
+    y += yr
+
+    # Approximate using Bezier spline.
+    degree = 3 if cp > 3 else cp - 1  # Degree of b-spline. 3 is recommended.
+                                      # Must be less than number of control points.
+    tck, u = scipy.interpolate.splprep([x, y], k=degree)
+    u = scipy.linspace(0, 1, num=max(pyautogui.size()))
+    points = scipy.interpolate.splev(u, tck)
+
+    # Move mouse.
+    duration = 0.2
+    timeout = duration / len(points[0])
+    for point in zip(*(i.astype(int) for i in points)):
+        pag.platformModule._moveTo(*point)
+        time.sleep(timeout)
+    
+#################################################################################
+
 
 def move_away(direction):
     """Moves the mouse to a random spot on right half or the left half of
