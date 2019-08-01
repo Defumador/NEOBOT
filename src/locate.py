723f38e0618ca2448339827f6bfa8061cdfc7f36
T. Austin Kelley <37026149+takelley1@users.noreply.github.com>
2019-08-01 13:49:41 -0400

    Refactored locate functions into single function
    This single locate function works whether it is given a haystack to look
    through or not.

--- src/locate.py
+++ src/locate.py
@@ -4,14 +4,13 @@ from src.vars import originx, originy, windowx, windowy
 
 import pyautogui as pag
 
-overviewx = (originx + (windowx - (int(windowx / 3.8))))
-overviewlx = (int(windowx / 3.8))
 
-
-def hslocate(needle, haystack=0, conf=0.95, grayscale=False):
-    """Searches the haystack image for the provided needle image, returns a tuple.
-    If a haystack image is not provided, search the client window instead."""
-    if haystack == 0:
+def mlocate(needle, haystack=0, conf=0.95, loctype='none', grayscale=False):
+    """Searches the haystack image for the needle image, returning a tuple of the needle's
+    coordinates within the haystack. If a haystack image is not provided, searches
+    the client window or the overview window, as specified by the loctype parameter."""
+    
+    if haystack == 0 and loctype == 'l':
         locate_var = pag.locateOnScreen(needle, confidence=conf, region=(originx, originy, windowx, windowy))
         if locate_var is not None:
             logging.debug('found image ' + (str(needle)))
@@ -20,7 +19,42 @@ def hslocate(needle, haystack=0, conf=0.95, grayscale=False):
             logging.debug('cannot find standard image ' + (
                     str(needle) + ' confidence is ' + (str(conf))))
             return 0
-    else:
+        
+    if haystack == 0 and loctype == 'c':
+        locate_var = pag.locateCenterOnScreen(needle, confidence=conf, region=(originx, originy, windowx, windowy))
+        if locate_var is not None:
+            logging.debug('found image ' + (str(needle)))
+            return locate_var
+        elif locate_var is None:
+            logging.debug('cannot find standard image ' + (
+                    str(needle) + ' confidence is ' + (str(conf))))
+            return 0
+        
+    if haystack == 0 and loctype == 'o':
+        overviewx = (originx + (windowx - (int(windowx / 3.8))))
+        overviewlx = (int(windowx / 3.8))
+        locate_var = pag.locateOnScreen(needle, confidence=conf, region=(overviewx, originy, overviewlx, windowy))
+        if locate_var is not None:
+            logging.debug('found image ' + (str(needle)))
+            return locate_var
+        elif locate_var is None:
+            logging.debug('cannot find standard image ' + (
+                    str(needle) + ' confidence is ' + (str(conf))))
+            return 0
+        
+    if haystack == 0 and loctype == 'o':
+        overviewx = (originx + (windowx - (int(windowx / 3.8))))
+        overviewlx = (int(windowx / 3.8))
+        locate_var = pag.locateCenterOnScreen(needle, confidence=conf, region=(overviewx, originy, overviewlx, windowy))
+        if locate_var is not None:
+            logging.debug('found image ' + (str(needle)))
+            return locate_var
+        elif locate_var is None:
+            logging.debug('cannot find standard image ' + (
+                    str(needle) + ' confidence is ' + (str(conf))))
+            return 0
+        
+    if haystack != 0 and loctype == 'none':
         locate_var = pag.locate(needle, haystack, confidence=conf, grayscale=grayscale)
         if locate_var is not None:
             logging.debug('found needle image ' + (str(needle)) + ' within haystack image' + (str(haystack)))
@@ -29,62 +63,7 @@ def hslocate(needle, haystack=0, conf=0.95, grayscale=False):
             logging.debug('cannot find image ' + (
                     str(needle) + ' confidence is ' + (str(conf))))
             return 0
-
-
-def locate(image, conf=0.95, region=(originx, originy, windowx, windowy)):
-    """Searches the client window for the provided image, returns a tuple."""
-    locate_var = pag.locateOnScreen(image, confidence=conf, region=region)
-    if locate_var is not None:
-        logging.debug('found image ' + (str(image)))
-        return locate_var
-    elif locate_var is None:
-        # logging.debug('cannot find image ' + (
-        #            str(image) + ' confidence is ' + (str(conf))))
-        return locate_var
-
-
-def clocate(image, conf=0.95, region=(originx, originy, windowx, windowy)):
-    """Searches the client window for the center coordinates of the provided
-    image, returns x and y coordinates."""
-    # 'clocate' = 'center locate'
-    clocate_var = pag.locateCenterOnScreen(image, confidence=conf,
-                                           region=region)
-    if clocate_var is not None:
-        logging.debug('found image ' + (str(image)))
-        return clocate_var
-    elif clocate_var is None:
-        # logging.debug('cannot find image ' + (
-        #            str(image) + ' confidence is ' + (str(conf))))
-        return clocate_var
-
-
-def olocate(image, conf=0.95, region=(overviewx, originy, overviewlx,
-                                      windowy)):
-    """Searches the rightmost quarter of the client window (the 'o' stands
-    for 'overview,' assuming the overview is attached to the right side of
-    the client). Searches for the provided image, returns a tuple"""
-    olocate_var = pag.locateOnScreen(image, confidence=conf, region=region)
-    if olocate_var is not None:
-        logging.debug('found image ' + (str(image)))
-        return olocate_var
-    elif olocate_var is None:
-        # logging.debug('cannot find image ' + (
-        #            str(image) + ' confidence is ' + (str(conf))))
-        return olocate_var
-
-
-def oclocate(image, conf=0.95, region=(overviewx, originy, overviewlx,
-                                       windowy)):
-    """Searches the rightmost quarter of the client window (the 'o' stands
-    for 'overview,' assuming the overview is attached to the right side of
-    the client). Searches for the center coordinates of the provided image,
-    returns x and y coordinates."""
-    oclocate_var = pag.locateCenterOnScreen(image, confidence=conf,
-                                            region=region)
-    if oclocate_var is not None:
-        logging.debug('found image ' + (str(image)))
-        return oclocate_var
-    elif oclocate_var is None:
-        # logging.debug('cannot find image ' + (
-        #            str(image) + ' confidence is ' + (str(conf))))
-        return oclocate_var
+        
+    else:
+        logging.error('incorrect function parameters')
+        sys.exit()
