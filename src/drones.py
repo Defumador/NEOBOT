8367907838c705b8022f2a5d830f71ffa9f4798f
takelley1 <37026149+Zultair@users.noreply.github.com>
2019-08-03 19:06:42 -0400

    Formatting
    Signed-off-by: takelley1 <37026149+Zultair@users.noreply.github.com>

--- src/drones.py
+++ src/drones.py
@@ -4,7 +4,6 @@
 import random
 import time
 import logging
-
 from src import keyboard as key, locate as lo
 
 
@@ -17,7 +16,8 @@ def launch_drones(drone_num):
         key.hotkey('shift', 'l')
 
         for tries in range(1, 25):
-            drones_launched = lo.mlocate('./img/indicators/drones/0_drone_in_bay.bmp')
+            drones_launched = lo.mlocate(
+                './img/indicators/drones/0_drone_in_bay.bmp')
             
             if drones_launched == 1:
                 logging.debug('drones launched ' + (str(tries)))
@@ -43,15 +43,17 @@ def recall_drones(drone_num):
         # Wait for all drones to return to drone bay. Very sensitive to the
         # drones variable. Won't work unless the drones variable is correct.
         for tries in range(1, 30):
-            drones_returned = lo.mlocate('./img/indicators/drones/' + (str(drone_num))
-                                                + '_drone_in_bay.bmp', grayscale=True)
+            drones_returned = lo.mlocate('./img/indicators/drones/' +
+                                         (str(drone_num)) +
+                                         '_drone_in_bay.bmp', grayscale=True)
 
             if drones_returned == 1:
                 logging.debug('drones returned to bay')
                 return 1
 
             elif drones_returned == 0:
-                logging.info('waiting for drones to return to bay ' + (str(tries)))
+                logging.info('waiting for drones to return to bay ' +
+                             (str(tries)))
                 time.sleep(float(random.randint(1000, 2000)) / 1000)
                 return 0
 
